8315804b7c42be7bf0fffc05a260a937d7bd42ab
João Rafael Santos <jrafael@e-goi.com>
2018-06-21 10:49:34 +0100

    Idempotent, Upsert and small fixes

--- test/actions/add_update_subscriber_test.js
+++ test/actions/add_update_subscriber_test.js
@@ -1,31 +1,31 @@
 const plg = require('pluga-plg');
 const expect = require('chai').expect;
 
-const action = require('../../lib/actions/update_subscriber');
+const action = require('../../lib/actions/add_update_subscriber');
 
 const subscriber = {
     first_name: 'João',
-    last_name: 'Santos',
-    email: 'example@e-goi.com'
+    last_name: 'Pluga',
+    email: 'joaosantos.pluga@e-goi.com'
 };
 
-describe('Action: Update subscriber', function () {
+describe('Action: Add/Update subscriber', function () {
 
-    it('update an existing subscriber and return it', function (done) {
+    it('creates/updates a subscriber and return it', function (done) {
         const event = {
             auth: {
                 api_key: process.env.API_KEY
             },
             input: {
                 listID: 5,
-                subscriber: subscriber.email,
-                first_name: subscriber.first_name
+                email: subscriber.email,
+                first_name: 'João'
             }
         };
 
         action.handle(plg, event)
             .then((res) => {
-                expect(res.first_name).to.eq(subscriber.first_name);
+                expect(res.email).to.eq(subscriber.email);
 
                 done();
             })
